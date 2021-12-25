4350f51df2e127af00a06321360c19293468db46
WilliamCMajor <she8@nait.ca>
2021-12-25 11:52:59 -0700

    add favicon

--- includes/header.php
+++ includes/header.php
@@ -12,6 +12,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
     <title><?php echo APP_NAME; ?></title>
+    <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
 
     <!-- Custom styles -->
     <link href="<?php echo BASE_URL ?>css/reset.css" rel="stylesheet">
