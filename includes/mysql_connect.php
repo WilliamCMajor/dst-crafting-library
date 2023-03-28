034b9ba8c552ba4c467ed5d33188693ec593907e
WilliamSiqing <71398644+WilliamCMajor@users.noreply.github.com>
2023-03-28 17:14:53 -0600

    Update mysql_connect.php

--- includes/mysql_connect.php
+++ includes/mysql_connect.php
@@ -31,7 +31,7 @@ if (mysqli_connect_errno()) {
 // 	We use a constant (basically the same as a variable (stores a value)) to set the Base URL. We will use this to resolve all links as absolute URL's instead of relative URL's. If we do this correct, then we will not have any troubles pathing to all our files.
   // Make sure the path starts with http:// and ends with the trailing slash
   // If you copy/paste this script into other projects and fail to change this, then you may have links going back to the former project. Copy/Paste/FAIL !
-  define("BASE_URL", "https://dst-carfting-library.herokuapp.com/");
+  define("BASE_URL", "https://dst-crafting-library.herokuapp.com/");
 
 
   // Your App Name
